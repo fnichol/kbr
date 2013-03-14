75fa35e213c916e569324cc3fadb07fd8e15c3cd
Fletcher Nichol <fnichol@nichol.ca>
2013-03-13 23:43:56 -0600

    Add OAuth authentication for higher rate limiting ceiling.

--- app.rb
+++ app.rb
@@ -16,10 +16,13 @@ module KBR
         builder.use :http_cache, *opts[:cache_opts]
         builder.adapter Faraday.default_adapter
       end
+      @client_id = opts[:client_id]
+      @client_secret = opts[:client_secret]
     end
 
     def tags(user, repo)
-      @connection.get("/repos/#{user}/#{repo}/git/refs/tags")
+      @connection.get("/repos/#{user}/#{repo}/git/refs/tags",
+        client_id: @client_id, client_secret: @client_secret)
     end
   end
 
@@ -28,26 +31,38 @@ module KBR
     configure do
       enable :logging
       set :redis_url, (ENV['REDIS_URL'] || "redis://127.0.0.1:6379")
+      set :gh_client_id, ENV['GITHUB_CLIENT_ID']
+      set :gh_client_secret, ENV['GITHUB_CLIENT_SECRET']
     end
 
     get '/tags/:user/:repo' do
       content_type :json
-
       response = github_client.tags(params[:user], params[:repo])
+      return response.status unless response.success?
 
-      if response.success?
-        response.body.map { |t| t["ref"].split("/").last }.sort.to_json
-      else
-        response.status
-      end
+      tags = response.body.map { |t| t["ref"].split("/").last }.sort
+      logger.info [
+        "repo=#{params[:user]}/#{params[:repo]}",
+        "rate_limit=#{rate_limit(response.headers)}",
+        "tags=#{tags.join(",")}",
+      ].join(" ")
+      tags.to_json
     end
 
     private
 
     def github_client
-      GitHubClient.new(cache_opts: [
-        :redis_store, settings.redis_url, logger: Logger.new(STDOUT)
-      ])
+      GitHubClient.new(
+        client_id: settings.gh_client_id,
+        client_secret: settings.gh_client_secret,
+        cache_opts: [
+          :redis_store, settings.redis_url, logger: Logger.new(STDOUT)
+        ]
+      )
+    end
+
+    def rate_limit(headers)
+      "#{headers['X-RateLimit-Remaining']}/#{headers['X-RateLimit-Limit']}"
     end
   end
 end
